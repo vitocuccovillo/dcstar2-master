9186a4d156557915fc654fefbcd7b112ffdd4f11
vitocuccovillo <vito.cuccovillo@gmail.com>
2017-10-04 08:54:22 +0200

    Adapter edited

--- test/SolutionAdapter.py
+++ test/SolutionAdapter.py
@@ -3,13 +3,10 @@ class SolutionAdapter(): #uniforma le soluzioni per renderle confrontabili
 
 
     def AdaptGASolution(self, GASolution):
-        GASolutionFlat = ""
-        for c in GASolution.candidate:
-            GASolutionFlat = GASolutionFlat + c
-        return GASolutionFlat
+        return GASolution
 
     def AdaptAStarSolution(self, AStarSolution):
-        AStarSolutionFlat = ""
+        AStarSolutionFlat = []
         for (city, cost) in AStarSolution:
-            AStarSolutionFlat = AStarSolutionFlat + city
+            AStarSolutionFlat.append(city)
         return AStarSolutionFlat
