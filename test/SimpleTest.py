eb5ee553c7abf59103190e9c97fb2788036a3959
vitoturing <cuccovito@gmail.com>
2017-10-05 18:25:54 +0200

    SimpleProblemTesting

--- test/SimpleTest.py
+++ test/SimpleTest.py
@@ -11,12 +11,13 @@ end_city = "J"
 # ----------- INIZIO -----------------
 
 solAdapter = SolutionAdapter(city_names)
-bestGASol = ["A","C","F","I","E","I","H","J"]
+#bestGASol = ["A","C","F","I","E","I","H","J"]
+bestGASol = ["A","B","D","G","H","J"]
 print("SOLUZIONE ALGORITMO GENETICO: " + str(bestGASol))
 
 dist = DistancePOI(bestGASol,levenshtein) # genera un oggetto DistancePoi prendendo in input la soluz. del GA ed una funzione
 
-#cp = SimpleCityProblem(start_city,end_city,dist, solAdapter) # costruisco il problema (fornisco in input le città e l'oggetto dist)
-cp = SimpleCityProblem(start_city,end_city,None, solAdapter) # costruisco il problema (fornisco in input le città e l'oggetto dist)
+cp = SimpleCityProblem(start_city,end_city,dist, solAdapter) # costruisco il problema (fornisco in input le città e l'oggetto dist)
+#cp = SimpleCityProblem(start_city,end_city,None, solAdapter) # costruisco il problema (fornisco in input le città e l'oggetto dist)
 solution = astar(cp)
 print("SOLUZIONE A*:" + str(solution))
